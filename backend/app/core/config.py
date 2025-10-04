ccdddb79a06eace1720e91c63516279000241da1
Ameet Toor <ameet2r@users.noreply.github.com>
2025-10-03 23:51:55 -0700

    Fix configs

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,10 +1,11 @@
 from pydantic_settings import BaseSettings
 from typing import List
+import os
 
 
 class Settings(BaseSettings):
-    FIREBASE_PROJECT_ID: str = ""
-    FIREBASE_PRIVATE_KEY_PATH: str = ""
+    FIREBASE_PROJECT_ID: str = os.getenv("FIREBASE_PROJECT_ID")
+    FIREBASE_PRIVATE_KEY_PATH: str = os.getenv("FIREBASE_PRIVATE_KEY_PATH")
     ALLOWED_ORIGINS: List[str] = ["http://localhost:5173", "http://localhost:3000"]
 
     class Config:
